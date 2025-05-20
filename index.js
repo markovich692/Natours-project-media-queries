168a184e8dab1f3f1d1104493c841499ccb1ed28
markovich692 <mark.devguru@outlook.com>
2025-05-20 10:20:15 -0500

    starts to work back on the media q ueries

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
-//Problems
-// Write code that enhances all arrays such that you can call
-// the array.last() method on any array and it will return the
-// last element. If there are no elements in the array, it should return -1.
+/*
+Given an integer n, return a counter function. This counter function
+initially returns n and then returns 1 more than the previous value 
+every subsequent time it is called (n, n + 1, n + 2, etc).
 
-// You may assume the array is the output of JSON.parse.
+Example 1:
 
-// Example 1:
+Input: 
+n = 10 
+["call","call","call"]
+Output: [10,11,12]
+Explanation: 
+counter() = 10 // The first time counter() is called, it returns n.
+counter() = 11 // Returns 1 more than the previous time.
+counter() = 12 // Returns 1 more than the previous time.
+Example 2:
 
-// Input: nums = [null, {}, 3]
-// Output: 3
-// Explanation: Calling nums.last() should return the last element: 3.
-// Example 2:
+Input: 
+n = -2
+["call","call","call","call","call"]
+Output: [-2,-1,0,1,2]
+Explanation: counter() initially returns -2. 
+Then increases after each sebsequent call.
+ 
+Constraints:
 
-// Input: nums = []
-// Output: -1
-// Explanation: Because there are no elements, return -1.
+-1000 <= n <= 1000
+0 <= calls.length <= 1000
+calls[i] === "call"
 
-// Constraints:
 
-// arr is a valid JSON array
-// 0 <= arr.length <= 1000
+var createCounter = function (n) {
+  let arr = [n];
 
-Array.prototype.last = function () {
-  const arrLength = this.length;
-  return 0 <= arrLength <= 1000 && arrLength ? this[arrLength - 1] : -1;
+  arr.forEach((el, i, arrayNum) => {
+    if (i === 0 || i !== arrayNum.length - 1) console.log(arr);
+
+    arr.push(el + 1);
+
+    // console.log(arr);
+  });
+
+  return function () {
+    arr;
+  };
 };
 
-console.log([1, 2, 4, 4, null].last());
+createCounter(23);
+
+console.log(createCounter());
+
+*/
